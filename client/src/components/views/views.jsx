6cad54ef0d5ab7c44884a230a96d530a82fcf953
MeLikeApplez <jeremisibayan@gmail.com>
2025-02-08 13:03:07 -0600

    Setting Up Views

--- client/src/components/views/views.jsx
+++ client/src/components/views/views.jsx
@@ -3,6 +3,13 @@ import './views.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function Views() {
-    return <div className="views"></div>
+export default function Views({ name, bio, description, buttonData }) {
+    return <div className="views">
+        <section>
+            <div className="name">{ name }</div>
+            <div className="bio">{ bio }</div>
+        </section>
+        <div className="description">{ description }</div>
+        <button className="submit">{ buttonData }</button>
+    </div>
 }
